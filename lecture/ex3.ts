778c4b02ed1e6434a485166d9f46e34c51f527aa
zeroday7 <zeroday7@daum.net>
2025-06-25 14:31:13 +0900

    ex3.ts 수정

--- lecture/ex3.ts
+++ lecture/ex3.ts
@@ -11,3 +11,6 @@ console.log("typeof person.birth:", typeof person.birth); // number
 console.log("typeof person.name:", typeof person.name); // string
 // console.log(person.home); // Error - '{ birth: number; name: string; }' 형식에 'home' 속성이 없습니다.
 
+/*
+    - 객체 타입 선언
+*/
